f7254d88b178304e4cffd64d3654ccb4c9f1047d
jisforjustin <jordan.justinb@gmail.com>
2016-11-20 15:02:54 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+"""******************************************************************
+File Name:main.py
+
+Authors: Justin Jordan, David Ikemba, Thuong Nguyen
+Date:
+Project: Machine Vision Burr Detection System
+Sponsor: Hunt and Hunt Ltd.
+Faculty Advisor: Dr. Fred Chen
+Instructor: Dr. Compeau
+University: Texas State University INgram School of Engineering
+
+Description: The Machine Vision Burr Detection System (MVBDS) will
+be designed by Texas State University Electrical Engineering
+students, for Hunt and Hunt Ltd., to detect burrs at the
+intersection of the keyway and inner-diameter threading of precision
+machined pipes. The system will generate a pass/fail signal to let
+the user know if burrs are detected. This is a proof of concept
+design to demonstrate that machine vision can be used to automate
+burr detection.
+
+Minimum Requirements: The MVBDS must comply with the following:
+
+•	Must detect defects within 1.5 minute time frame
+•	Must generate pass/fail signal with 95% accuracy or greater
+•	Must use Hunt & Hunt Ltd. Machined pipes
+•	Must detect defects 1mm or larger
+•	Project budget needs to be $500 or less
+******************************************************************"""
+
+print("Hello world!")
